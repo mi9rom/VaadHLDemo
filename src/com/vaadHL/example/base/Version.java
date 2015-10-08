e951ffd74480d76e5a9ddeca903bd17d729bb7f9
mi9rom <mi9rom@gmail.com>
2015-10-08 23:25:43 +0200

    ver nr

--- src/com/vaadHL/example/base/Version.java
+++ src/com/vaadHL/example/base/Version.java
@@ -20,7 +20,7 @@ package com.vaadHL.example.base;
  * Version information.
  */
 public class Version {
-	private final static String version = "1.3.02";
+	private final static String version = "1.4.01";
 
 	/**
 	 * Gets full version string.
